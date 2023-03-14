27646d62ac72424e989ee1e03fdb3a99c2d72b88
github-action-benchmark <github@users.noreply.github.com>
2023-03-14 18:01:43 +0000

    add Benchmark (ndjson) benchmark result for da46c1f0dcb991617aa45833728dfb6e18f9e624

--- perf/results/j_converterperf/hebrew/UTF-8/TestCharsetEncoderICU/data.js
+++ perf/results/j_converterperf/hebrew/UTF-8/TestCharsetEncoderICU/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678328824019,
+  "lastUpdate": 1678816902942,
   "repoUrl": "https://github.com/3246251196/icu",
   "entries": {
     "Benchmark": [
@@ -2518,6 +2518,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestCharsetEncoderICU",
             "value": 3.4613669842270474,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "3246251196ryan@gmail.com",
+            "name": "rjd",
+            "username": "3246251196"
+          },
+          "committer": {
+            "email": "3246251196ryan@gmail.com",
+            "name": "rjd",
+            "username": "3246251196"
+          },
+          "distinct": true,
+          "id": "da46c1f0dcb991617aa45833728dfb6e18f9e624",
+          "message": "Move to previous release 72.1",
+          "timestamp": "2023-03-14T17:45:19Z",
+          "tree_id": "31681c4f13f050551be1280ec41de39b9d454cb8",
+          "url": "https://github.com/3246251196/icu/commit/da46c1f0dcb991617aa45833728dfb6e18f9e624"
+        },
+        "date": 1678816894434,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestCharsetEncoderICU",
+            "value": 3.350854788806331,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
