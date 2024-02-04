4ee333f709e815587a72a5fd12486a39a70f6c18
github-action-benchmark <github@users.noreply.github.com>
2024-02-04 20:30:27 +0000

    add Benchmark (ndjson) benchmark result for a23195914ede6f65361283842fec348f4a687925

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_NFD_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707074020883,
+  "lastUpdate": 1707078627098,
   "repoUrl": "https://github.com/3246251196/icu",
   "entries": {
     "Benchmark": [
@@ -2998,6 +2998,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_NFD_Text",
             "value": 2.944350208774098,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "3246251196ryan@gmail.com",
+            "name": "rjd324",
+            "username": "3246251196"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a23195914ede6f65361283842fec348f4a687925",
+          "message": "Update rjdMake",
+          "timestamp": "2024-02-04T20:09:13Z",
+          "tree_id": "78ad6ff5c734a872ed00319bb43b31f191ef93a5",
+          "url": "https://github.com/3246251196/icu/commit/a23195914ede6f65361283842fec348f4a687925"
+        },
+        "date": 1707078620778,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_NFD_Text",
+            "value": 2.985212114134277,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
