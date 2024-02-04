bdfeb6beeda486b87b75b7f5b1c778f531b0e4f3
github-action-benchmark <github@users.noreply.github.com>
2024-02-04 19:09:50 +0000

    add Benchmark (ndjson) benchmark result for ff9a8a4727e0816ebcf1bed24c70e4344fc28c79

--- perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
+++ perf/results/j_decimalformatperf/de_DE/TestICUFormat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704597976643,
+  "lastUpdate": 1707073790452,
   "repoUrl": "https://github.com/3246251196/icu",
   "entries": {
     "Benchmark": [
@@ -2878,6 +2878,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUFormat",
             "value": 547.6858437619289,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "3246251196ryan@gmail.com",
+            "name": "rjd324",
+            "username": "3246251196"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff9a8a4727e0816ebcf1bed24c70e4344fc28c79",
+          "message": "Update rjdMake",
+          "timestamp": "2024-02-04T18:58:55Z",
+          "tree_id": "03b1195238ebaae33cd656e7c533707f12ee5f5c",
+          "url": "https://github.com/3246251196/icu/commit/ff9a8a4727e0816ebcf1bed24c70e4344fc28c79"
+        },
+        "date": 1707073784270,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUFormat",
+            "value": 554.4629549506805,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
